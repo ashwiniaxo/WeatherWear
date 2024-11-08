b43adf0d51b067b5334afa6e8f7da3a230d6c7ce
Ashwinia <ashwinia1233@hotmail.com>
2024-11-07 23:50:05 -0500

    Update serveur.js

--- api/serveur.js
+++ api/serveur.js
@@ -5,7 +5,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+//const PORT = 3000;
+const PORT = process.env.PORT || 4000 
 const apiKey = process.env.MY_KEY;
 
 app.use(express.static('public', {
